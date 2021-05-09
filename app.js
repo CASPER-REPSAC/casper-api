bb86a8b18191c1de4fdfbbb1834e979e1fc0245e
gsniper777 <gsniper777@gmail.com>
2021-05-09 13:07:46 +0000

    app.js

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const session = require("express-session");
 const app = express();
 
+app.get('/',(req,res){
+    res.send("Casper API Test");
+});
+
 app.listen("7777", ()=>{
     console.log("Server Start")
 });
