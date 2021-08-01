ed67c0515a9f50928f55f9470a0e3352fe517f7d
woohyun212 <woohyun212@gmail.com>
2021-08-02 08:43:14 +0900

    Tutorial-3 Applied.
    views fixed.
    urls fixed.
    ------------------------------------
    코드가 이렇게 간결해질 수 있음에 감탄.

--- activity/urls.py
+++ activity/urls.py
@@ -3,7 +3,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from activity import views
 
 urlpatterns = [
-    path('activities/', views.activityList),
-    path('activities/<int:pk>/', views.activityDetail),
+    path('activities/', views.ActivityList().as_view()),
+    path('activities/<int:pk>/', views.ActivityDetail.as_view()),
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
--- activity/views.py
+++ activity/views.py
@@ -1,49 +1,13 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from activity.models import Activity
 from activity.serializers import ActivitySerializer
+from rest_framework import generics
 
 
-@api_view(['GET', 'POST'])
-def activityList(request, format=None):
-    """
-    List all activities, or create a new activity.
-    """
-    if request.method == 'GET':
-        activities = Activity.objects.all()
-        serializer = ActivitySerializer(activities, many=True)
-        return Response(serializer.data)
-
-    elif request.method == 'POST':
-        serializer = ActivitySerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class ActivityList(generics.ListCreateAPIView):
+    queryset = Activity.objects.all()
+    serializer_class = ActivitySerializer
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def activityDetail(request, pk, format=None):
-    """
-    Retrieve, update or delete a activity.
-    """
-    try:
-        activity = Activity.objects.get(pk=pk)
-    except Activity.DoesNotExist:
-        return HttpResponse(status.HTTP_400_BAD_REQUEST)
-
-    if request.method == 'GET':
-        serializer = ActivitySerializer(activity)
-        return Response(serializer.data)
-
-    elif request.method == 'PUT':
-        serializer = ActivitySerializer(activity, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        activity.delete()
-        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+class ActivityDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Activity.objects.all()
+    serializer_class = ActivitySerializer
