9348af4dd7fcb2da4b4e2a2e072251c4879f6a35
woohyun212 <woohyun212@gmail.com>
2021-08-02 00:53:37 +0900

    ModelSerializer 사용할 거라면 create, update 선언 안해도됨

--- activity/serializers.py
+++ activity/serializers.py
@@ -2,26 +2,30 @@ from rest_framework import serializers
 from activity.models import Activity
 
 
-class ActivitySerializer(serializers.Serializer):
+class ActivitySerializer(serializers.ModelSerializer):
     class Meta:
         model = Activity
         fields = ['id', 'title', 'type', 'author', 'createDate', 'introduce',
                   'startDate', 'endDate', 'currentState', 'viewerNum']
 
-    def create(self, validated_data):
-        """
-        Create and return a new `Snippet` instance, given the validated data.
-        """
-        return Activity.objects.create(**validated_data)
+    # def create(self, validated_data):
+    #     """
+    #     Create and return a new `Activity` instance, given the validated data.
+    #     """
+    #     return Activity.objects.create(**validated_data)
+    #
+    # def update(self, instance, validated_data):
+    #     """
+    #     Update and return an existing `Activity` instance, given the validated data.
+    #     """
+    #     instance.title = validated_data.get("title", instance.title)
+    #     instance.type = validated_data.get("type", instance.type)
+    #     instance.author = validated_data.get("author", instance.author)
+    #     instance.createDate = validated_data.get("createDate", instance.createDate)
+    #     instance.introduce = validated_data.get("introduce", instance.introduce)
+    #     instance.startDate = validated_data.get("startDate", instance.startDate)
+    #     instance.endDate = validated_data.get("endDate", instance.endDate)
+    #     instance.currentState = validated_data.get("currentState", instance.currentState)
+    #     instance.save()
+    #     return instance
 
-    def update(self, instance, validated_data):
-        """
-        Update and return an existing `Snippet` instance, given the validated data.
-        """
-        instance.title = validated_data.get('title', instance.title)
-        instance.code = validated_data.get('code', instance.code)
-        instance.linenos = validated_data.get('linenos', instance.linenos)
-        instance.language = validated_data.get('language', instance.language)
-        instance.style = validated_data.get('style', instance.style)
-        instance.save()
-        return instance
