2f25bbb6671f926857952e6198f433300d5ef658
Floodnut <gsniper777@gmail.com>
2021-10-09 00:05:09 +0900

    delete Method

--- managing/models.py
+++ managing/models.py
@@ -9,13 +9,50 @@ from django.db import models
 
 class Activity(models.Model):
     id = models.AutoField(primary_key=True)
-    author = models.CharField(max_length=16)
-    title = models.CharField(max_length=32)
-    category = models.CharField(max_length=8)
+    title = models.CharField(max_length=50)
+    type_CHOICES = (
+        ('CTF', 'CTF'),
+        ('Study', 'Study'),
+        ('Project', 'Project'),
+    )
+    type = models.CharField(max_length=50, choices=type_CHOICES)
+    author = models.CharField(max_length=50)
+    # owner = models.ForeignKey('auth.User', related_name='activities', on_delete=models.CASCADE)
+    # author 를 owner 로 교체해야할 때가 올 것임..
+    createDate = models.DateField(db_column='createDate')
+    description = models.CharField(max_length=65)
+    startDate = models.DateField(db_column='startDate')
+    endDate = models.DateField(db_column='endDate')
+    currentState_CHOICES = (
+        (0, '0 : 예정'),
+        (1, '1 : 진행'),
+        (2, '2 : 종료'),
+    )
+    currentState = models.PositiveIntegerField(db_column='currentState', default=0,
+                                               choices=currentState_CHOICES)
+    viewerNum = models.PositiveIntegerField(db_column='viewerNum', default=0)
+
 
     class Meta:
         managed = False
         db_table = 'activity'
+
+    def __str__(self):
+        return self.title
+
+class Chapter(models.Model):
+    activityid = models.ForeignKey(Activity, related_name='chapterid',db_column='activityid', on_delete=models.CASCADE)#related_name='chapterid', on_delete=models.CASCADE)
+    chapterid = models.AutoField(primary_key=True)
+    subject = models.CharField(max_length=16)
+    created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
+    article = models.CharField(max_length=500)
+    filepath = models.CharField(max_length=32, blank=True, null=True)
+    filesize = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'chapter'
 
 class Chapter(models.Model):
     activityid = models.ForeignKey(Activity, related_name='chapterid',db_column='activityid', on_delete=models.CASCADE)#related_name='chapterid', on_delete=models.CASCADE)
