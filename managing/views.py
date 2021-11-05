0d3091ee97c30255a1089472974c516325f6c439
woohyun212 <woohyun212@gmail.com>
2021-11-06 04:02:36 +0900

    본영이의 요청 사항 - 해결
    ### activity_list 에서 태그와 사람의 이름이 보였으면 좋겠음.
    - addTagName - serializer.data 에서 tag_id 로 tag_name 을 찾아서 추가함.
    - addTagName - serializer.data 에서 user_id 로 user_name 을 찾아서 추가함.
    
    ## activity_list 로 activity 생성할 때, tag 도 입력하면 생성됐으면 좋겠음.
    - request 로 activity 가 생성되면, request 에서 tags 를 따로 떼어내어 처리함.
    -- tags 는 리스트 자료형으로 입력받음, 입력받은 태그가 없으면 db에 새로 생성함
    -- tag 가 다 존재하면, activity 와 tag 를 연결하는 actitag 를 객체를 생성함.
    ### post 의 created 응답에는 tag_name 이 보이지 않음
    -- 리얼라이저를 재정의해서 데이터를 다시가져오는 것으로 해결.

--- managing/views.py
+++ managing/views.py
@@ -9,20 +9,29 @@ from rest_framework.response import Response
 from rest_framework.parsers import MultiPartParser, FormParser  # for file upload
 from rest_framework import viewsets, serializers, status
 
-from .serializers import ActivitySerializer, ChapterSerializer, ChapterListSerializer, ChaptercommentSerializer, \
-    ChapterfileSerializer, ActivityListSerializer
-# from activity.serializers import ActivitySerializer
+from activity.serializers import TagSerializer
+from .serializers import *
 from .models import Activity, Chapter, Chaptercomment, Chapterfile
 from activity.models import *
 
+from rest_framework.utils.serializer_helpers import ReturnDict, ReturnList
 
+
+####start#####
+### 공사중 ###
 def addTagName(response_data):
     tag_list = Tag.objects.all()
-    print("태그 데이터 : ")
-    for d_idx, _object in enumerate(response_data):
-        for o_idx, tag in enumerate(_object['tags']):
-            response_data[d_idx]['tags'][o_idx]['tag_name'] = str(tag_list.get(pk=tag['tag_id']))
-    return response_data
+    # post 로 생성할 때는 response_data 의 데이터 타입이 다름.
+    # get 으로 불러올 떄는 ReturnDict 가 list 로 묶여있어서 ReturnList 이 되나봄.
+    # 그래서 아래와 같이 따로 처리 해줌.
+    if type(response_data) == ReturnDict:
+        for o_idx, tag_id in enumerate(response_data['tags']):
+            response_data['tags'][o_idx]['tag_name'] = str(tag_list.get(pk=tag_id['tag_id']))
+    else:
+        for d_idx, _object in enumerate(response_data):
+            for o_idx, tag in enumerate(_object['tags']):
+                response_data[d_idx]['tags'][o_idx]['tag_name'] = str(tag_list.get(pk=tag['tag_id']))
+    # return response_data # 리턴이 없어도 serializer.data 가 수정됨.
 
 
 def addUserName(response_data):
@@ -30,7 +39,8 @@ def addUserName(response_data):
     for d_idx, _object in enumerate(response_data):
         for o_idx, tag in enumerate(_object['participants']):
             response_data[d_idx]['participants'][o_idx]['user_name'] = str(user_list.get(pk=tag['user_id']))
-    return response_data
+    # return response_data # 리턴이 없어도 serializer.data 가 수정됨.
+######end#####
 
 
 @api_view(['GET', 'POST'])
@@ -39,20 +49,37 @@ def activity_list(request):
     if request.method == "GET":
         activities = Activity.objects.all()
         serializer = ActivityListSerializer(activities, many=True, context=context)
-        ##########
-        # 공사중 #
-        ##########
-        response_data = addTagName(serializer.data)
-        response_data = addUserName(response_data)
-        ##########
-        # 공사중 #
-        ##########
-        return Response(response_data)
+
+        ####start#####
+        ### 공사중 ###
+        addTagName(serializer.data)
+        addUserName(serializer.data)
+        ######end#####
+
+        return Response(serializer.data)
 
     elif request.method == "POST":
-        serializer = ActivitySerializer(data=request.data, context=context)
+        serializer = ActivityListSerializer(data=request.data, context=context)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
+
+            ####start#####
+            ### 공사중 ###
+            if 'tags' in request.data:
+                activity_instance = Activity.objects.get(pk=serializer.data['id']) # 방금 생성된 activity_instance 가져옴
+                req_tags = request.data['tags']  # post 로 입력받은 태그 리스트
+                exist_tags = [tag.name for tag in Tag.objects.all()]  # 존재하는 태그 가져옴
+                for r_tag in req_tags:
+                    if r_tag not in exist_tags:  # 없으면 태그 db 에 등록
+                        Tag(name=r_tag).save()
+                    tag_instance = Tag.objects.get(name=r_tag)  # 태그 인스턴스 찾아옴
+                    ActivityTag(activity_id=activity_instance, tag_id=tag_instance).save()  # activity 랑 tag 연결
+                # 문제 발견, 태그 생성은 되는데 response 에 보이질 않음.
+                serializer = ActivityListSerializer(activity_instance, context=context)
+                # 시리얼라이저를 재정의해서 데이터를 다시가져오는 것으로 해결.
+                addTagName(serializer.data)
+            ######end#####
+
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -63,6 +90,13 @@ def activity_detail(request, pk):
         context = {'request': request}
         activity = Activity.objects.filter(id=pk)
         serializer = ActivitySerializer(activity, many=True, context=context)
+
+        ####start#####
+        ### 공사중 ###
+        addTagName(serializer.data)
+        addUserName(serializer.data)
+        ######end#####
+
         return Response(serializer.data)
 
     elif request.method == "POST":
