a3edeb37fd8775fb1b7b1a29b5be01d4b93f8ebf
Floodnut <gsniper777@gmail.com>
2021-11-11 23:18:28 +0900

    File Upload 1.0

--- managing/models.py
+++ managing/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from activity.models import *
+import connects.settings as sett
 
 class Chapter(models.Model):
     activityid = models.ForeignKey(Activity, related_name='chapterid',db_column='activityid', on_delete=models.CASCADE)#related_name='chapterid', on_delete=models.CASCADE)
